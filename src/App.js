096324e4282ff4cead289a50e903dc952594cd26
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-07 12:44:22 +0100

    Create Banner component and mount on to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Banner from './components/Banner';
 import './App.css';
 
 class App extends Component {
-  // render() {
-  //   return (
-  //   );
-  // }
+  render() {
+    return (
+      <Banner />
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Banner = () => {
+  return (
+    <h1>Hello World!</h1>
+  );
+}
+
+export default Banner;
