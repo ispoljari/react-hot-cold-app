f19b4d95d204b7a4339e9f45ec4155c94c188eff
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-07 12:47:57 +0100

    Add empty feedback component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Banner from './components/Banner';
+import Feedback from './components/Feedback';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Banner />
+      <div>
+        <Banner />
+        <Feedback />
+      </div>
     );
   }
 }
---
+++ src/components/Feedback.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Feedback = () => {
+  return (
+    <h2>Feedback Component</h2>
+  );
+}
+
+export default Feedback;
