2911c2ad9cff5208981d088ed65e7e1081e611f5
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-09 09:54:56 +0100

    Add rel=noopener noreferrer to Author comp a links which contain target=_blank

--- src/components/Author.js
+++ src/components/Author.js
@@ -11,12 +11,12 @@ const Author = () => {
       </div>
       <div className="author__social">
         <div className="social__linkedin">
-          <a href="https://www.linkedin.com/in/ivan-špoljarić-2206a184" target="_blank" >
+          <a href="https://www.linkedin.com/in/ivan-špoljarić-2206a184" target="_blank" rel="noopener noreferrer">
             <img src={linkedinLogo} alt="LinkedIn logo small icon" />
           </a>
         </div>
         <div className="social__github">
-          <a href="https://github.com/ispoljari" target="_blank" >
+          <a href="https://github.com/ispoljari" target="_blank" rel="noopener noreferrer">
             <img src={githubLogo} alt="Github logo small icon" />
           </a>
         </div>
