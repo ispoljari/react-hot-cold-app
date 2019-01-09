14062736645ae62f9607796765824db52bd7991c
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-09 09:23:26 +0100

    Destructure props in Reset component

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Reset.css';
 
-const Reset = props => {
-  return (
-    <div className="text-center reset">
-      <button className="btn btn-success btn-block reset__btn" type="button" onClick={props.resetGame()}>
-        Reset Game
-      </button>
-    </div>
-  );
-}
+const Reset = ({resetGame}) => (
+  <div className="text-center reset">
+    <button className="btn btn-success btn-block reset__btn" type="button" onClick={resetGame()}>
+      Reset Game
+    </button>
+  </div>
+);
 
 export default Reset;
