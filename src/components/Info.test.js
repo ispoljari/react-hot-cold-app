4debdb93d20a030cf2cf00a5fd7c5b8604325bf8
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-10 17:54:14 +0100

    Add test case to Info.test suite which checks if the callback gets invoked and executed on info btn click

--- src/components/Info.test.js
+++ src/components/Info.test.js
@@ -8,4 +8,14 @@ describe('<Info />', () => {
   it('Renders without crashing', () => {
     shallow(<Info />);
   });
+
+  // check if handleShow gets called on btn click
+  it('Should call handleShow on btn click and change state', () => {
+    const wrapper = shallow(<Info />);
+    expect(wrapper.state('show')).toBe(false);
+    wrapper.find('button').simulate('click', {
+      preventDefault() {}
+    });
+    expect(wrapper.state('show')).toBe(true);
+  });
 });
