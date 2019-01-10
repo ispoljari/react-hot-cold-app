a674bfd143b9c6888f89e35ae15386f5ad7e8655
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-10 17:19:36 +0100

    Add first smoke test to Info.test test suite

--- src/components/Info.test.js
+++ src/components/Info.test.js
@@ -2,3 +2,10 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import Info from './Info';
+
+describe('<Info />', () => {
+  // smoke test
+  it('Renders without crashing', () => {
+    shallow(<Info />);
+  });
+});
