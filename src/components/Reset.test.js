9d4edc23ebd07735c31c5a797e91626e693b0a07
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-10 17:06:55 +0100

    Add test case to Reset.test test suit which checks if callback gets called on click

--- src/components/Reset.test.js
+++ src/components/Reset.test.js
@@ -14,4 +14,14 @@ describe('<Reset />', () => {
     const wrapper = shallow(<Reset resetGame='fn'/>);
     expect(wrapper.find('button').props().onClick).toEqual('fn');
   });
+
+  // check if callback gets called on click event
+  it('Should call resetGame callback when button is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = shallow(<Reset resetGame={callback}/>);
+    wrapper.find('button').simulate('click', {
+      preventDefault() {}
+    });
+    expect(callback).toHaveBeenCalled();
+  });
 });
