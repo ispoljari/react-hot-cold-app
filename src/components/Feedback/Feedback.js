1749c54af2d31980fc052a340cd60cc08081a54d
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-14 17:16:30 +0100

    Refactor Feedback css as styled component

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import './Feedback.css';
+import { Typography } from '@smooth-ui/core-sc';
+import styled from 'styled-components';
+
+const StyledFeedback = styled.div`
+  border-radius: 5px;
+  padding: 4px;
+  background-color: #ea4335;
+  display: flex;
+  align-items: center;
+  min-height: 50px;
+`;
 
 const Feedback = ({feedback}) =>  (
-  <div className="feedback">
-    <h2 className="text-center feedback__value">{feedback}</h2>
-  </div>
+  <StyledFeedback className="feedback">
+    <Typography variant="h2" fontSize={22} textAlign="center" my={0} mx="auto">       {feedback}
+    </Typography>
+  </StyledFeedback>
 );
 
 export default Feedback;
