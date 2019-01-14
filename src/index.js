fc7339d01ef70ca50658b1c774d4510b4e13f51a
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-14 14:55:46 +0100

    Setup global normalize with <GlobalStyle />

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { globalStyle, createGlobalStyle } from '@smooth-ui/core-sc';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`${globalStyle()}`;
+
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root')
+);
