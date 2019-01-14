b557d9acdba8292c11d18657a1b9c5302ee261e5
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-14 16:12:03 +0100

    Refactor Banner Typography element to use utility 'm' instead of inline marginTop

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Typography } from '@smooth-ui/core-sc';
 
 const Banner = () => (
-  <Typography variant="h1" textAlign="center" style={{margin: "0"}}>HOT or COLD</Typography>
+  <Typography variant="h1" textAlign="center" m={0}>
+    HOT or COLD
+  </Typography>
 );
 
 export default Banner;
