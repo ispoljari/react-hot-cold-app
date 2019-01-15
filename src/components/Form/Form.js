622650b0fa23fe58e9e43c288e6c0dab0e301a86
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 11:53:52 +0100

    Separate Form comp styles into a diff module and import

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Input, Button } from '@smooth-ui/core-sc';
-
-const StyledForm = styled.form`
-  margin-top: 10px;
-`;
+import * as Styled from './style';
 
 const Form = ({block, returnGuessToApp}) => {
   const onSubmit = e => {
@@ -19,12 +15,12 @@ const Form = ({block, returnGuessToApp}) => {
   }
   
   return (
-    <StyledForm onSubmit={onSubmit}>
+    <Styled.Form onSubmit={onSubmit}>
       <Input width={1} type="number" textAlign="center" fontSize={22} minHeight={40} name="guess" min="1" max="100" placeholder="Enter your guess" required/>
       <Button variant="success" width={1} mt={10} minHeight={40} fontSize={22} type="submit">
         Guess
       </Button>
-    </StyledForm>
+    </Styled.Form>
   );
 }
 
