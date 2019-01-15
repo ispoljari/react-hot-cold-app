20b10bf211d61415f495f11a338f28f0d43f504e
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 09:59:26 +0100

    Replace all styles in Author component with styled components

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import linkedinLogo from '../../svg/linkedin.svg';
 import githubLogo from '../../svg/github.svg';
-import './Author.css';
 import styled from 'styled-components';
 import { Typography } from '@smooth-ui/core-sc';
 
 const StyledAuthor = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const StyledSocial = styled(StyledAuthor)`
+  margin-top: 5px;
+
+  img {
+    width: 35px;
+    display: block;
+  }
+`;
+
+const StyledLinkedIn = styled.div``;
+const StyledGitub = styled(StyledLinkedIn)`
+  margin-left: 10px;
 `;
 
 const Author = () => (
@@ -17,18 +30,18 @@ const Author = () => (
         <small>Developed by</small> <strong>Ivan Špoljarić</strong>
       </Typography>
     </StyledAuthor>
-    <div className="author__social">
-      <div className="social__linkedin">
+    <StyledSocial>
+      <StyledLinkedIn>
         <a href="https://www.linkedin.com/in/ivan-špoljarić-2206a184" target="_blank" rel="noopener noreferrer">
           <img src={linkedinLogo} alt="LinkedIn logo small icon" />
         </a>
-      </div>
-      <div className="social__github">
+      </StyledLinkedIn>
+      <StyledGitub>
         <a href="https://github.com/ispoljari" target="_blank" rel="noopener noreferrer">
           <img src={githubLogo} alt="Github logo small icon" />
         </a>
-      </div>
-    </div>
+      </StyledGitub>
+    </StyledSocial>
   </div>
 )
 
