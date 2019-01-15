1492297734fbaf02af1de550fbcbbdbf5df489ce
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 10:13:47 +0100

    Replace styles in Progress comp with sc's

--- src/components/Progress/Progress.js
+++ src/components/Progress/Progress.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import './Progress.css';
+import { Typography } from '@smooth-ui/core-sc';
+import styled from 'styled-components';
+
+const StyledProgress = styled.div`
+  margin-top: 10px;
+`;
+const StyledHistory = styled.ul`
+  display: flex;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  min-height: 30px;
+  padding: 5px;
+  list-style: none;
+  color: black;
+  font-size: 24px;
+
+  li {
+    color: #fff;
+    margin-right: 8px;
+    padding: 2px;
+    border-radius: 4px;
+  }
+`;
 
 const Progress = ({attempt, guessList}) => (
-  <div className="progressBar">
-    <h2 className="text-center progressBar__current">Guess #{attempt}</h2>
-    <ul className="progressBar__history">
+  <StyledProgress>
+    <Typography variant="h2" textAlign="center" color="black">
+      Guess #{attempt}
+    </Typography>
+    <StyledHistory>
       {guessList}
-    </ul>
-  </div>
+    </StyledHistory>
+  </StyledProgress>
 );
 
 export default Progress;
