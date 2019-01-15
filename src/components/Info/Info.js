611254d3a80c5c8e33a5772cd0b10b8bf3bb52bb
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 10:46:41 +0100

    Replace styles in Info comp with sc's and smooth-ui

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
-import './Info.css';
-import Modal from 'react-bootstrap/lib/Modal';
-import Button from 'react-bootstrap/lib/Button';
+
+import {
+  Modal,
+  ModalDialog,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button, 
+  Typography
+} from '@smooth-ui/core-sc';
+
+import styled from 'styled-components';
+
+const StyledInfo = styled.div`
+  margin-top: 5px;
+`;
 
 class Info extends Component {
   state = {
@@ -23,40 +37,46 @@ class Info extends Component {
 
   render() {
     return (
-      <div className="text-center info">
-        <button className="btn btn-info btn-block info__btn" type="button" onClick={this.handleShow}>
+      <StyledInfo>
+        <Button variant="secondary" width={1} mt={10} minHeight={40} fontSize={22} type="button" onClick={this.handleShow}>
           How To Play
-        </button>
-        <Modal show={this.state.show} onHide={this.handleClose} className="info__modal">
-          <Modal.Header closeButton>
-            <Modal.Title>
-              Game Rules
-            </Modal.Title>
-          </Modal.Header>
-  
-          <Modal.Body>
-            This is a Number Guessing Game with the following rules:
-            <br />
-            <br />
-            1. The machine picks a random number between 1 to 100 and keeps it hidden.
-            <br />
-            <br />
-            2. You need to guess until you can find the hidden secret number.
-            <br />
-            <br />
-            3. You will get feedback on how close or far your guess is in the form of the following keywords ("cold", "warm", "hot", "extremely hot").
-            <br />
-            <br />
-            Everything clear?
-          </Modal.Body>
-  
-          <Modal.Footer>
-            <Button onClick={this.handleClose}>
-              YES
-            </Button>
-          </Modal.Footer>
+        </Button>
+        <Modal opened={this.state.show} onClose={this.handleClose}>
+          <ModalDialog>
+            <ModalContent>
+
+              <ModalHeader>
+                <Typography variant="h2" textAlign="center" color="black">
+                  Game Rules
+                </Typography>
+              </ModalHeader>
+      
+              <ModalBody fontSize={20}>
+                This is a Number Guessing Game with the following rules:
+                <br />
+                <br />
+                1. The machine picks a random number between 1 to 100 and keeps it hidden.
+                <br />
+                <br />
+                2. You need to guess until you can find the hidden secret number.
+                <br />
+                <br />
+                3. You will get feedback on how close or far your guess is in the form of the following keywords ("cold", "warm", "hot", "extremely hot").
+                <br />
+                <br />
+                Everything clear?
+              </ModalBody>
+      
+              <ModalFooter>
+                <Button variant="success" size="md" width={0.3} mt={10} minHeight={40} fontSize={22} type="button" onClick={this.handleClose}>
+                  YES
+                </Button>
+              </ModalFooter>
+              
+            </ModalContent>
+          </ModalDialog>
         </Modal>
-      </div>
+      </StyledInfo>
     );
   }
 }
