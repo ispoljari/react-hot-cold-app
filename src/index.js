92953ebac0d59238b1f03d202415003416cb74ee
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 13:16:21 +0100

    Separate Global  styles into a diff module and import

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { globalStyle, createGlobalStyle } from '@smooth-ui/core-sc';
-import background from './img/background.jpg';
+import * as Styled from './global-style';
 
 import App from './App';
 
-const GlobalStyle = createGlobalStyle`
-  ${globalStyle()}
-  body {
-    color: white;
-    background: url(${background}) white; 
-  }
-`;
-
 ReactDOM.render(
   <React.Fragment>
-    <GlobalStyle />
+    <Styled.Global />
     <App />
   </React.Fragment>,
   document.getElementById('root')
