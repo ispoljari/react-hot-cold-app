5dbad7725f08776cc7e1b3bfc83ea04187436eef
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-01-15 12:01:06 +0100

    Unistall boostrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { globalStyle, createGlobalStyle } from '@smooth-ui/core-sc';
+
 import App from './App';
 
 const GlobalStyle = createGlobalStyle`${globalStyle()}`;
